1ba804e2a72d3665f1c0654606065587c3f75866
iswenzz <suxlolz1528@gmail.com>
2019-06-29 16:48:19 +0200

    UtilString: Function to check if a function call is threaded

--- Parser/Util/UtilString.cs
+++ Parser/Util/UtilString.cs
@@ -36,5 +36,26 @@ namespace Iswenzz.CoD4.Parser.Util
         /// <param name="comparisonType">Comparison Type</param>
         public static bool Contains(this string line, string value, StringComparison type) =>
             line?.IndexOf(value, type) >= 0;
+
+        /// <summary>
+        /// Returns a value indicating whether a function call is threaded with the "thread" keyword.
+        /// </summary>
+        /// <param name="funcIndex">Start index of the function call</param>
+        /// <returns>The start index of the "thread" keyword</returns>
+        public static int IsLineThreaded(this string line, int funcIndex)
+        {
+            while (line[funcIndex--] != ';')
+            {
+                if (funcIndex - 5 >= 0)
+                {
+                    // thread
+                    if (line[funcIndex - 5] == 't' && line[funcIndex - 4] == 'h' && line[funcIndex - 3] == 'r'
+                        && line[funcIndex - 2] == 'e' && line[funcIndex - 1] == 'a' && line[funcIndex] == 'd')
+                        return funcIndex - 5;
+                }
+                else return -1;
+            }
+            return -1;
+        }
     }
 }
